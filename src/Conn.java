2403e0c67ec155df14d539a2b1ee02a0a269e4c5
ritviksingh791 <f20220045@pilani.bits-pilani.ac.in>
2024-04-10 23:04:01 +0530

    Hi

--- src/Conn.java
+++ src/Conn.java
@@ -9,8 +9,7 @@ public class Conn {
         try {
             String user = "root";
             // get password from environment variable MYSQL_ROOT_PASSWORD
-//            String password = System.getenv("MYSQL_ROOT_PASSWORD");
-            String password = "Ritvik@2005";
+            String password = System.getenv("MYSQL_ROOT_PASSWORD");
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/campuspay", user, password);
             stmt = con.createStatement();
