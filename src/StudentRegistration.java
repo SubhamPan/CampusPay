a901fca926f632b82283e23ba31fd220e2e0c4ca
ritviksingh791 <f20220045@pilani.bits-pilani.ac.in>
2024-04-13 20:51:35 +0530

    Hi

--- src/StudentRegistration.java
+++ src/StudentRegistration.java
@@ -4,97 +4,70 @@ import java.awt.event.*;
 import java.sql.*;
 
 public class StudentRegistration {
-    // this class is used to register a student
-    private Container c;
-    private JLabel title;
-    private JLabel name;
-    private JTextField nameText;
-    private JLabel ID;
-    private JTextField IDText;
-    private JLabel Contact;
-    private JTextField ContactText;
-    private JLabel password;
-    private JPasswordField passwordText;
-    private JButton submit;
-    private JButton back;
-    private JTextArea tout;
-
     public void show() {
-        // create a new frame to store the registration form
-        JFrame f = new JFrame("Register Student");
-        f.setSize(600, 400);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setLayout(null);
+        JFrame frame = new JFrame("Register Student");
+        frame.setBounds(300, 90, 900, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
 
-        // create a label
-        title = new JLabel("Register Student");
+        Container container = frame.getContentPane();
+        container.setBackground(new Color(243, 238, 234));
+
+        JLabel title = new JLabel("Register Student");
         title.setFont(new Font("Arial", Font.PLAIN, 30));
-        title.setSize(300, 30);
-        title.setLocation(250, 30);
-        f.add(title);
+        title.setBounds(350, 100, 300, 30);
+        container.add(title);
 
-        // create a label for the name
-        name = new JLabel("Name");
-        name.setFont(new Font("Arial", Font.PLAIN, 20));
-        name.setSize(100, 20);
-        name.setLocation(100, 100);
-        f.add(name);
+        JLabel nameLabel = new JLabel("Name");
+        nameLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        nameLabel.setBounds(300, 170, 100, 20);
+        container.add(nameLabel);
 
-        // create a text field for the name
-        nameText = new JTextField();
+        JTextField nameText = new JTextField();
         nameText.setFont(new Font("Arial", Font.PLAIN, 15));
-        nameText.setSize(190, 20);
-        nameText.setLocation(200, 100);
-        f.add(nameText);
+        nameText.setBounds(400, 170, 190, 20);
+        nameText.setBorder(BorderFactory.createLineBorder(new Color(224, 227, 215), 2));
+        container.add(nameText);
 
-        // create a label for the ID
-        ID = new JLabel("ID");
-        ID.setFont(new Font("Arial", Font.PLAIN, 20));
-        ID.setSize(100, 20);
-        ID.setLocation(100, 150);
-        f.add(ID);
+        JLabel idLabel = new JLabel("ID");
+        idLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        idLabel.setBounds(300, 220, 100, 20);
+        container.add(idLabel);
 
-        // create a text field for the ID
-        IDText = new JTextField();
-        IDText.setFont(new Font("Arial", Font.PLAIN, 15));
-        IDText.setSize(190, 20);
-        IDText.setLocation(200, 150);
-        f.add(IDText);
+        JTextField idText = new JTextField();
+        idText.setFont(new Font("Arial", Font.PLAIN, 15));
+        idText.setBounds(400, 220, 190, 20);
+        idText.setBorder(BorderFactory.createLineBorder(new Color(224, 227, 215), 2));
+        container.add(idText);
 
-        // create a label for the contact
-        Contact = new JLabel("Contact");
-        Contact.setFont(new Font("Arial", Font.PLAIN, 20));
-        Contact.setSize(100, 20);
-        Contact.setLocation(100, 200);
-        f.add(Contact);
+        JLabel contactLabel = new JLabel("Contact");
+        contactLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        contactLabel.setBounds(300, 270, 100, 20);
+        container.add(contactLabel);
 
-        // create a text field for the contact
-        ContactText = new JTextField();
-        ContactText.setFont(new Font("Arial", Font.PLAIN, 15));
-        ContactText.setSize(190, 20);
-        ContactText.setLocation(200, 200);
-        f.add(ContactText);
+        JTextField contactText = new JTextField();
+        contactText.setFont(new Font("Arial", Font.PLAIN, 15));
+        contactText.setBounds(400, 270, 190, 20);
+        contactText.setBorder(BorderFactory.createLineBorder(new Color(224, 227, 215), 2));
+        container.add(contactText);
 
-        // create a label for the password
-        password = new JLabel("Password");
-        password.setFont(new Font("Arial", Font.PLAIN, 20));
-        password.setSize(100, 20);
-        password.setLocation(100, 250);
-        f.add(password);
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        passwordLabel.setBounds(300, 320, 100, 20);
+        container.add(passwordLabel);
 
-        // create a text field for the password
-        passwordText = new JPasswordField();
+        JPasswordField passwordText = new JPasswordField();
         passwordText.setFont(new Font("Arial", Font.PLAIN, 15));
-        passwordText.setSize(190, 20);
-        passwordText.setLocation(200, 250);
-        f.add(passwordText);
+        passwordText.setBounds(400, 320, 190, 20);
+        passwordText.setBorder(BorderFactory.createLineBorder(new Color(224, 227, 215), 2));
+        container.add(passwordText);
 
-        // create a button to submit the form
-        submit = new JButton("Submit");
-        submit.setFont(new Font("Arial", Font.PLAIN, 15));
-        submit.setSize(100, 20);
-        submit.setLocation(150, 300);
-        submit.addActionListener(new ActionListener() {
+        JButton submitButton = new JButton("Submit");
+        submitButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        submitButton.setBounds(325, 370, 100, 30);
+        submitButton.setBackground(new Color(176, 166, 149));
+        submitButton.setBorder(BorderFactory.createLineBorder(new Color(176, 166, 149), 2));
+        submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // make a connection to the database
@@ -102,47 +75,58 @@ public class StudentRegistration {
                 try {
                     // use procedure register_student(IN ID varchar(50), IN BITS_account varchar(50), IN s_name varchar(50), IN contact char(10), IN password varchar(256))
                     CallableStatement cs = c.con.prepareCall("{call register_student(?, ?, ?, ?, ?)}");
-                    cs.setString(1, IDText.getText());
-                    cs.setString(2, IDText.getText());
+                    cs.setString(1, idText.getText());
+                    cs.setString(2, idText.getText());
                     cs.setString(3, nameText.getText());
-                    cs.setString(4, ContactText.getText());
+                    cs.setString(4, contactText.getText());
                     String password = new String(passwordText.getPassword());
                     password = Hash.hash(password);
                     cs.setString(5, password);
                     cs.execute();
 
                     // show a success message
-                    JOptionPane.showMessageDialog(f, "Student registered successfully");
+                    JOptionPane.showMessageDialog(frame, "Student registered successfully");
 
                     // go back to the login page
                     LoginSystem login = new LoginSystem();
                     login.show();
-                    f.dispose();
+                    frame.dispose();
 
                 } catch (Exception ex) {
                     System.out.println(ex);
                 }
             }
         });
-        f.add(submit);
+        container.add(submitButton);
 
-        // create a button to go back to the LoginSystem
-        back = new JButton("Back");
-        back.setFont(new Font("Arial", Font.PLAIN, 15));
-        back.setSize(100, 20);
-        back.setLocation(270, 300);
-        back.addActionListener(new ActionListener() {
+        JButton backButton = new JButton("Back");
+        backButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        backButton.setBounds(475, 370, 100, 30);
+        backButton.setBackground(new Color(176, 166, 149));
+        backButton.setBorder(BorderFactory.createLineBorder(new Color(176, 166, 149), 2));
+        backButton.addActionListener(new ActionListener() {
+            // Add your back button logic here
             @Override
             public void actionPerformed(ActionEvent e) {
                 // go back to the login page
                 LoginSystem login = new LoginSystem();
                 login.show();
-                f.dispose();
+                frame.dispose();
             }
         });
-        f.add(back);
+        container.add(backButton);
 
-        // set the frame visibility
-        f.setVisible(true);
+        frame.setVisible(true);
+
+        // Centering the form within the frame
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (dim.width - frame.getSize().width) / 2;
+        int y = (dim.height - frame.getSize().height) / 2;
+        frame.setLocation(x, y);
+    }
+
+    public static void main(String[] args) {
+        StudentRegistration studentRegistration = new StudentRegistration();
+        studentRegistration.show();
     }
 }
