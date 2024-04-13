f516dc5fe45e61b6e606b1d4c22d8bdef2fdb533
ritviksingh791 <f20220045@pilani.bits-pilani.ac.in>
2024-04-13 21:22:20 +0530

    Hi

--- src/StudentHome.java
+++ src/StudentHome.java
@@ -4,148 +4,124 @@ import java.awt.event.*;
 import java.sql.*;
 
 public class StudentHome {
-    // show the home page
     public void show() {
-        // create a new frame to store text field and button
-        JFrame f = new JFrame("Home");
-        f.setSize(800, 600);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setLayout(null);
+        JFrame frame = new JFrame("Home");
+        frame.setBounds(300, 90, 900, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
 
-        // create a label
+        Container container = frame.getContentPane();
+        container.setBackground(new Color(243, 238, 234));
+
         JLabel title = new JLabel("Home Page");
         title.setFont(new Font("Arial", Font.PLAIN, 30));
-        title.setSize(300, 30);
-        title.setLocation(250, 30);
-        f.add(title);
+        title.setBounds(375, 30, 300, 30);
+        container.add(title);
 
-        // show the total amount spent by querying the database
-        JLabel total = new JLabel("Total Amount Spent: ");
-        total.setFont(new Font("Arial", Font.PLAIN, 20));
-        total.setSize(150, 20);
-        total.setLocation(100, 100);
-        f.add(total);
+        JLabel totalLabel = new JLabel("Total Amount Spent: ");
+        totalLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        totalLabel.setBounds(325, 100, 200, 20);
+        container.add(totalLabel);
 
-        // connect to the database and get the total amount spent
-        // use the procedure get_total_amount_spent_by_student(IN student_id varchar(50), OUT total_amount_spent int)
+        JLabel totalAmount = new JLabel();
+        totalAmount.setFont(new Font("Arial", Font.PLAIN, 20));
+        totalAmount.setBounds(515, 100, 150, 20);
+        container.add(totalAmount);
+
         try {
-            Conn c = new Conn();
-            CallableStatement cs = c.con.prepareCall("{call get_total_amount_spent_by_student(?, ?)}");
-            cs.setString(1, User.getInstance().getId());
-            cs.registerOutParameter(2, Types.INTEGER);
-            cs.execute();
-            int total_amount_spent = cs.getInt(2);
-            JLabel totalAmount = new JLabel(Integer.toString(total_amount_spent));
-            totalAmount.setFont(new Font("Arial", Font.PLAIN, 20));
-            totalAmount.setSize(150, 20);
-            totalAmount.setLocation(250, 100);
-            f.add(totalAmount);
+//            Conn c = new Conn();
+//            CallableStatement cs = c.con.prepareCall("{call get_total_amount_spent_by_student(?, ?)}");
+//            cs.setString(1, User.getInstance().getId());
+//            cs.registerOutParameter(2, Types.INTEGER);
+//            cs.execute();
+//            int total_amount_spent = cs.getInt(2);
+            int total_amount_spent = 1000;
+            totalAmount.setText(Integer.toString(total_amount_spent));
         } catch (Exception e) {
             System.out.println(e);
         }
 
-        // vendors page button
-        JButton vendors = new JButton("Vendors");
-        vendors.setFont(new Font("Arial", Font.PLAIN, 15));
-        vendors.setSize(100, 20);
-        vendors.setLocation(250, 150);
-        vendors.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // show the list of vendors
-                VendorsMenu vendors = new VendorsMenu();
-                vendors.show();
-                f.dispose();
-            }
+        JButton vendorsButton = new JButton("Vendors");
+        vendorsButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        vendorsButton.setBounds(400, 150, 100, 30);
+        vendorsButton.setBackground(new Color(176, 166, 149));
+        vendorsButton.setBorder(BorderFactory.createLineBorder(new Color(176, 166, 149), 2));
+        vendorsButton.addActionListener(e -> {
+            VendorsMenu vendorsMenu = new VendorsMenu();
+            vendorsMenu.show();
+            frame.dispose();
         });
-        f.add(vendors);
+        container.add(vendorsButton);
 
-        // payment page button
-        JButton payment = new JButton("Payment");
-        payment.setFont(new Font("Arial", Font.PLAIN, 15));
-        payment.setSize(100, 20);
-        payment.setLocation(250, 200);
-        payment.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // show the payment form
-                Payment payment = new Payment();
-                payment.show();
-                f.dispose();
-            }
+        JButton paymentButton = new JButton("Payment");
+        paymentButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        paymentButton.setBounds(400, 200, 100, 30);
+        paymentButton.setBackground(new Color(176, 166, 149));
+        paymentButton.setBorder(BorderFactory.createLineBorder(new Color(176, 166, 149), 2));
+        paymentButton.addActionListener(e -> {
+            Payment payment = new Payment();
+            payment.show();
+            frame.dispose();
         });
-        f.add(payment);
+        container.add(paymentButton);
 
-        // payment history page button
-        JButton paymentHistory = new JButton("Payment History");
-        paymentHistory.setFont(new Font("Arial", Font.PLAIN, 15));
-        paymentHistory.setSize(150, 20);
-        paymentHistory.setLocation(250, 250);
-        paymentHistory.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // show the payment history
-                StudentPaymentHistory paymentHistory = new StudentPaymentHistory();
-                paymentHistory.show();
-                f.dispose();
-            }
+        JButton paymentHistoryButton = new JButton("Payment History");
+        paymentHistoryButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        paymentHistoryButton.setBounds(375, 250, 150, 30);
+        paymentHistoryButton.setBackground(new Color(176, 166, 149));
+        paymentHistoryButton.setBorder(BorderFactory.createLineBorder(new Color(176, 166, 149), 2));
+        paymentHistoryButton.addActionListener(e -> {
+            StudentPaymentHistory paymentHistory = new StudentPaymentHistory();
+            paymentHistory.show();
+            frame.dispose();
         });
-        f.add(paymentHistory);
+        container.add(paymentHistoryButton);
 
-        // update student details page button
-        JButton updateDetails = new JButton("Update Details");
-        updateDetails.setFont(new Font("Arial", Font.PLAIN, 15));
-        updateDetails.setSize(150, 20);
-        updateDetails.setLocation(250, 300);
-        updateDetails.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // show the update student details form
-                EditStudent editStudent = new EditStudent();
-                editStudent.show();
-                f.dispose();
-            }
+        JButton updateDetailsButton = new JButton("Update Details");
+        updateDetailsButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        updateDetailsButton.setBounds(375, 300, 150, 30);
+        updateDetailsButton.setBackground(new Color(176, 166, 149));
+        updateDetailsButton.setBorder(BorderFactory.createLineBorder(new Color(176, 166, 149), 2));
+        updateDetailsButton.addActionListener(e -> {
+            EditStudent editStudent = new EditStudent();
+            editStudent.show();
+            frame.dispose();
         });
-        f.add(updateDetails);
+        container.add(updateDetailsButton);
 
-        // create a button to show orders
-        JButton orders = new JButton("Show Orders");
-        orders.setFont(new Font("Arial", Font.PLAIN, 15));
-        orders.setSize(200, 20);
-        orders.setLocation(200, 350);
-        orders.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ShowOrders showOrders = new ShowOrders();
-                showOrders.show();
-                f.dispose();
-            }
+        JButton ordersButton = new JButton("Show Orders");
+        ordersButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        ordersButton.setBounds(375, 350, 150, 30);
+        ordersButton.setBackground(new Color(176, 166, 149));
+        ordersButton.setBorder(BorderFactory.createLineBorder(new Color(176, 166, 149), 2));
+        ordersButton.addActionListener(e -> {
+            ShowOrders showOrders = new ShowOrders();
+            showOrders.show();
+            frame.dispose();
         });
-        f.add(orders);
+        container.add(ordersButton);
 
-        // create a button
-        JButton logout = new JButton("Logout");
-        logout.setFont(new Font("Arial", Font.PLAIN, 15));
-        logout.setSize(100, 20);
-        logout.setLocation(250, 500);
-        logout.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // go back to the login page
-                User.getInstance().clear();
-                LoginSystem login = new LoginSystem();
-                login.show();
-                f.dispose();
-            }
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        logoutButton.setBounds(400, 450, 100, 30);
+        logoutButton.setBackground(new Color(176, 166, 149));
+        logoutButton.setBorder(BorderFactory.createLineBorder(new Color(176, 166, 149), 2));
+        logoutButton.addActionListener(e -> {
+//            User.getInstance().clear();
+            LoginSystem login = new LoginSystem();
+            login.show();
+            frame.dispose();
         });
-        f.add(logout);
+        container.add(logoutButton);
 
-        // set the frame visibility
-        f.setVisible(true);
+        frame.setVisible(true);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (dim.width - frame.getSize().width) / 2;
+        int y = (dim.height - frame.getSize().height) / 2;
+        frame.setLocation(x, y);
     }
+//    public static void main(String[] args) {
+//        StudentHome studentHome = new StudentHome();
+//        studentHome.show();
+//    }
 }
